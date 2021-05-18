51a59a3e28f092e7cb5d53ce52e685abe76b5bc2
Pavel Žák <pavel@zak.global>
2021-05-18 10:46:16 +0200

    Run jobs without a pool in test environment

--- packages/djorm-cloud-jobs/localRunner.js
+++ packages/djorm-cloud-jobs/localRunner.js
@@ -1,19 +1,20 @@
+const path = require('path')
+
 const { error, info } = require('djorm/logger')
 const { formatMessage } = require('./pubsub')
 const { getEntrypoint } = require('./entry')
 const { pool } = require('workerpool')
+const { getSettings } = require('djorm/config')
 
 /** Run the job as a cascade of workers in a pool.
  *
  * @async
- * @param {string} topic Job topic
- * @param {Object} message Serialized job instance
+ * @param {string} topic
+ * @param {Object} message
  * @return void
  */
-const runLocalJob = async (topic, message) => {
-  info(`SPAWN: ${topic}: ${JSON.stringify(message)}`)
+const runPoolJob = async (topic, message) => {
   const entry = getEntrypoint(topic)
-  const path = require('path')
   const poolPath = path.join(__dirname, 'localJob.js')
   const jobPool = pool(poolPath)
   await jobPool
@@ -24,6 +25,23 @@ const runLocalJob = async (topic, message) => {
       error(e.stderr)
     })
   jobPool.terminate()
+}
+
+/** Run the job in local environment
+ *
+ * @async
+ * @param {string} topic Job topic
+ * @param {Object} message Serialized job instance
+ * @return void
+ */
+const runLocalJob = async (topic, message) => {
+  info(`SPAWN: ${topic}: ${JSON.stringify(message)}`)
+  if (getSettings('cloudJobs.pool', process.env.NODE_ENV !== 'test')) {
+    return await runPoolJob(topic, message)
+  }
+  return await require(getEntrypoint(topic)).runJob({
+    data: formatMessage(message)
+  })
 }
 
 module.exports = {
