b42e4caf74db17802d0a99d41c0c66b1984fe87a
Pavel Žák <pavel@zak.global>
2021-06-01 14:03:45 +0200

    Silence logger in tests

--- jest.afterEnv.js
+++ jest.afterEnv.js
@@ -1,4 +1,10 @@
 const moment = require('moment-timezone')
+const { configure } = require('djorm/config')
 
 moment.tz.setDefault('UTC')
 moment.suppressDeprecationWarnings = true
+configure({
+  logger: {
+    level: 'error'
+  }
+})
