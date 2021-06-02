0569706d46394e1787aa9acd83684fc4cbdbcb95
Pavel Žák <pavel@zak.global>
2021-06-03 00:32:41 +0200

    Configure docs tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 const { guessRootConfig } = require('lerna-jest')
 
 module.exports = guessRootConfig(__dirname)
+
+const docs = module.exports.projects.find(
+  item => item.name === 'djorm-docs-linter'
+)
+
+docs.testPathIgnorePatterns.push('/build/')
