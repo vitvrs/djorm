a12468b1ab6598165dfb334b6d29c90bdd625488
Pavel Žák <pavel@zak.global>
2021-06-30 11:01:13 +0200

    Extract updateInstanceValues

--- packages/djorm/db/DatabaseMapper.js
+++ packages/djorm/db/DatabaseMapper.js
@@ -11,28 +11,34 @@ class DatabaseMapper extends Transform {
     return null
   }
 
+  static updateInstanceValues (inst, values, prefix) {
+    const entries = Object.entries(values)
+    for (const [fieldName, fieldValue] of entries) {
+      const fieldNameStripped = this.parseFieldName(fieldName, prefix)
+      if (fieldNameStripped) {
+        try {
+          inst.setFromDb(fieldNameStripped, fieldValue)
+        } catch (e) {
+          // Avoid killing mapper by parsing errors
+          inst.set(fieldNameStripped, null)
+          // @TODO: Warn about this error!
+        }
+      }
+    }
+  }
+
+  static mapInstanceValues (Model, values, prefix) {
+    const inst = new Model()
+    this.updateInstanceValues(inst, values, prefix)
+    return inst
+  }
+
   static createMapper = Model => {
     if (!Model) {
       return null
     }
     const prefix = `${getModelName(Model)}__`
-    return item => {
-      const inst = new Model()
-      const entries = Object.entries(item)
-      for (const [fieldName, fieldValue] of entries) {
-        const fieldNameStripped = this.parseFieldName(fieldName, prefix)
-        if (fieldNameStripped) {
-          try {
-            inst.setFromDb(fieldNameStripped, fieldValue)
-          } catch (e) {
-            // Avoid killing mapper by parsing errors
-            inst.set(fieldNameStripped, null)
-            // @TODO: Warn about this error!
-          }
-        }
-      }
-      return inst
-    }
+    return values => this.mapInstanceValues(Model, values, prefix)
   }
 
   constructor (qs) {
