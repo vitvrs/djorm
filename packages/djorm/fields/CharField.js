fbea349316103cd7699ea4d31f8ee107a38fa4d9
Pavel Žák <pavel@zak.global>
2021-09-21 16:57:04 +0200

    Parse CharField values as string

--- packages/djorm/fields/CharField.js
+++ packages/djorm/fields/CharField.js
@@ -1,6 +1,10 @@
 const { TrivialField } = require('./TrivialField')
 
 /** Field used for char values */
-class CharField extends TrivialField {}
+class CharField extends TrivialField {
+  parse (value, inst) {
+    return String(value)
+  }
+}
 
 module.exports = { CharField }
