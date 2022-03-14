b3194ba74d36a2f799850664ca2d6b8b637b3a23
Pavel Žák <pavel@zak.global>
2022-03-14 13:31:32 +0100

    feat: filter sqlite query array

--- packages/djorm-db-sqlite/SqliteFormatter.js
+++ packages/djorm-db-sqlite/SqliteFormatter.js
@@ -1,5 +1,6 @@
 const { SqlFormatter } = require('djorm-db-sql')
 const { QueryArray } = require('djorm/db/QueryArray')
+const { QueryColumn } = require('djorm/db/QueryColumn')
 
 class SqliteFormatter extends SqlFormatter {
   escapeChar (char) {
@@ -10,6 +11,25 @@ class SqliteFormatter extends SqlFormatter {
       return '\\'
     }
     return super.escapeChar(char)
+  }
+
+  formatConditionExpression (qs, condition, fieldSpec, value) {
+    if (value instanceof QueryArray) {
+      const fieldName = fieldSpec.replace(this.operatorMatch, '')
+      const col = new QueryColumn(fieldName)
+      if (!col.source) {
+        col.props.source = qs.props.target
+      }
+      const field = this.formatQueryColumn(col, true)
+      return `${value.value
+        .map(value => {
+          const formattedValue =
+            typeof value === 'string' ? `"${value}"` : value
+          return `${field} GLOB '*${formattedValue}[],]*'`
+        })
+        .join(' AND ')}`
+    }
+    return super.formatConditionExpression(qs, condition, fieldSpec, value)
   }
 
   formatValue (value) {
