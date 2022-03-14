6e10b9c376e51db218f5fe9f90ee4384d920fea3
Pavel Žák <pavel@zak.global>
2022-03-14 13:35:21 +0100

    feat: filter mysql query array

--- packages/djorm-db-mysql/MysqlFormatter.js
+++ packages/djorm-db-mysql/MysqlFormatter.js
@@ -1,7 +1,27 @@
 const { SqlFormatter } = require('djorm-db-sql')
 const { QueryArray } = require('djorm/db/QueryArray')
+const { QueryColumn } = require('djorm/db/QueryColumn')
 
 class MysqlFormatter extends SqlFormatter {
+  formatConditionExpression (qs, condition, fieldSpec, value) {
+    if (value instanceof QueryArray) {
+      const fieldName = fieldSpec.replace(this.operatorMatch, '')
+      const col = new QueryColumn(fieldName)
+      if (!col.source) {
+        col.props.source = qs.props.target
+      }
+      const field = this.formatQueryColumn(col, true)
+      return `${value.value
+        .map(value => {
+          const formattedValue =
+            typeof value === 'string' ? `"${value}"` : value
+          return `${field} REGEXP '${formattedValue}[\\],]'`
+        })
+        .join(' AND ')}`
+    }
+    return super.formatConditionExpression(qs, condition, fieldSpec, value)
+  }
+
   formatValue (value) {
     return super.formatValue(
       value instanceof QueryArray ? JSON.stringify(value.value) : value
