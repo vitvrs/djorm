ad9d1a4f58aa074faed8422350634cfac0fbf866
Pavel Žák <pavel@zak.global>
2023-06-09 12:07:18 +0200

    chore: avoid postinstall in production

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,16 +1,18 @@
-const {
-  installDatastore,
-  installSdk
-} = require('../packages/__jest__/datastore')
+if (process.env.NODE_ENV !== 'production') {
+  const {
+    installDatastore,
+    installSdk
+  } = require('../packages/__jest__/datastore')
 
-const main = async () => {
-  try {
-    await installSdk()
-    await installDatastore()
-  } catch (e) {
-    console.error(e)
-    process.exit(255)
+  const main = async () => {
+    try {
+      await installSdk()
+      await installDatastore()
+    } catch (e) {
+      console.error(e)
+      process.exit(255)
+    }
   }
-}
 
-main()
+  main()
+}
